6275a7117bcab0c1508e8de8a43122872fdafd95
Netto Pro <enzomnf_pro@outlook.com>
2025-04-08 22:03:33 -0300

    starting JPA/Hibernate study with Maven build

---
+++ src/main/java/test/basic/NewUser.java
@@ -0,0 +1,26 @@
+package test.basic;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import model.basic.User;
+
+public class NewUser {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence
+            .createEntityManagerFactory("exercises-jpa");
+
+        EntityManager em = emf.createEntityManager();
+
+        User newUser = new User("Johnny","johnny@yahoo.com");
+        newUser.setId(1L);
+
+        em.getTransaction().begin();
+        em.persist(newUser);
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+    }
+}
